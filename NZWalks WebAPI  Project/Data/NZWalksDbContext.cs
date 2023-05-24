80248a6a5c061d4c521b2be539b0358a491ea9ce
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-05-24 13:23:29 +0300

    NZWalks-WebAPI-Project

---
+++ NZWalks WebAPI  Project/Data/NZWalksDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks_WebAPI__Project.Models.Domain;
+
+namespace NZWalks_WebAPI__Project.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        { }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+    }
+}
---
+++ NZWalks WebAPI  Project/Models/Domain/Walk.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks_WebAPI__Project.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string WalkImageUrl { get; set; }
+        public Guid? RegionId { get; set; }
+        public Guid? DifficultyId { get; set; }
+        public virtual Region Region { get; set; }
+        public virtual Difficulty Difficulty { get; set; }
+    }
+}
